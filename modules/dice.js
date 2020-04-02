82d2cee7359693b7ca0910fa0d5b33b8c300c348
Me-k_01 <mecaman01@gmail.com>
2020-04-02 03:37:08 +0200

    Styling 1

--- modules/dice.js
+++ modules/dice.js
@@ -1,8 +1,9 @@
 
 function Dice (nMax = 6) {
   var self = this;
-  nMax = parseInt(nMax, 10);
+  nMax = parseInt(nMax); // Nombre de Face
   self.number = Math.trunc( Math.random() * nMax ) + 1;  // Face visible du dé
+  self.w = 100; // taille de l'image du dé.
 
   switch (self.number) {
     case 1:
@@ -12,22 +13,22 @@ function Dice (nMax = 6) {
       self.state = "Success";
       break;
     default:
-      self.state = "Default";
+      self.state = "";
+  }
+
+  switch (nMax) {
+    case 6:
+      self.shape = "rect";
+      break;
+    case 4:
+      self.shape = "oct"; //TODO: triangle
+      break;
+    default:
+      self.shape = "oct";
   }
 
   self.draw = (ctx, x, y) => {
-    switch (self.state) {
-      case "Fail":
-        ctx.fillStyle = "#EEE111";
-        break;
-      case "Success":
-        ctx.fillStyle = "#111EEE";
-        break;
-      default:
-        ctx.fillStyle = "#AAAAAA";
-
-    }
-
-    ctx.fillText(self.number, x, y);
+    ctx.drawImage(document.getElementById(self.shape + self.state), x, y, self.w, self.w);
+    ctx.fillText(self.number, x+self.w/2, y+self.w/2+3);
   };
 }
--- modules/main.js
+++ modules/main.js
@@ -1,32 +1,57 @@
 
 
-function RollSet () {
-  /* Objet de lancé de dés*/
+function RollSet() {
+  /* Lancement d'une certaines quantité de dés*/
   var self = this;
   const cv  = document.getElementById('canvas');
+  cv.width = (window.innerWidth * 70) / 100;
+  cv.height = window.innerHeight - (window.innerHeight * 40) / 100;
+  /* Stylisation du context 2D du canvas */
   const ctx = cv.getContext('2d');
+  ctx.fillStyle = '#1d1d20';
+  ctx.textAlign = "center";
+  ctx.font = '1.5em serif';
 
+  // Quantité de dé a lancer
   self.quantity = document.getElementById('quantity').value;
-  self.nFaces = document.getElementById('faces').value;  // Nombre de Faces
+  // Nombre de Faces
+  self.nSides = document.getElementById('faces').value;
+  // Influence des echecs et reussite critiques sur le nombre total de lancé de dés.
+  var critInfluence = document.getElementById('influence').checked;
   self.total = 0;
-
   self.list = [];  // Liste des dés obtenus
 
   self.draw = () => {
+    let xInit, yInit, x, y;
+    xInit = x = 30;
+    yInit = y = 40;
+
     ctx.clearRect(0, 0, cv.width, cv.height); // On efface ce qu'il y avait avant
 
     for ( let i=0; i < self.list.length; i++) {
       let d = self.list[i];
-      d.draw(ctx, i*50, 100);  // On dessine chacun des dés sur le canvas
+      if ( x > cv.width-d.w ) {
+        x = xInit;
+        y += d.w;
+      }
+      d.draw(ctx, x, y);  // On dessine chacun des dés sur le canvas
+      x += d.w;
     }
   };
 
   for ( let i=0; i<self.quantity; i++ ) {
-    let d = new Dice(self.nFaces);
+    let d = new Dice(self.nSides);
     self.total += d.number;
-
     self.list.push(d);
 
+    if (critInfluence) {
+      if (d.number == 1) {
+        self.quantity --;
+      }
+      if (d.number == self.nSides) {
+        self.quantity ++;
+      }
+    }
   }
 
   self.avg = self.total / self.quantity;  // Moyenne des lancés
@@ -34,7 +59,7 @@ function RollSet () {
 
 }
 
-var historic = [];
+var historic = [];  // Variable global de l'historique des lancé
 
 function mkRoll(){
   historic.push(new RollSet());
